2ae8f5a12857df9a26a29d99c8140bfef83f1f01
harshita2629 <54017759+harshita2629@users.noreply.github.com>
2020-08-11 12:13:24 +0530

    Update IndividualServices.py

--- IndividualServices.py
+++ IndividualServices.py
@@ -1,6 +1,6 @@
 import os
 
-raw = input("Enter the Service name: ")
+raw = input("Enter the Service name to check it's status: ")
 s='systemctl is-active '  + raw
 status = os.system(s)
 print(status)
